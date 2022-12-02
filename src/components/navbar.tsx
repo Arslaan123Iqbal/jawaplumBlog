78640247c5f1ed2d0dd0622228e2dff2002b9877
Arslan123Iqbal <arslaniqbalmgt@gmail.com>
2022-12-02 18:15:29 +0500

    Changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Link from "next/link";  
 import styles from '../../styles/Navbar.module.scss';
-
+import leaf from '../assets/Vector.svg'
+import Image from 'next/image';
 const Navbar = () => {
     return (
         <nav className={styles.main}>
             <div className={styles.logo}>
-            JAWAPLUM|BLOG
+            JAWAPLUM|    <Image
+      
+      src={leaf}
+      alt="Picture of the author"
+      width={25}
+      height={25}
+    />
            </div>
-           <div>Blog</div>
+           <div>Contacts</div>
            <Link href="/" className={styles.link}>
                 Visit
             </Link>
